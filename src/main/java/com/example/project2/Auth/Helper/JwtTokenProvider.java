3d74a9f66bd94a16f508caaadfb198d5df6893f0
kien@kien <kien.nguyen@sotatek.com>
2021-04-06 18:00:28 +0700

    --amend

---
+++ src/main/java/com/example/project2/Auth/Helper/JwtTokenProvider.java
@@ -0,0 +1,4 @@
+package com.example.project2.Auth.Helper;
+
+public class JwtTokenProvider {
+}
--- src/main/java/com/example/project2/Config/WebSecurityConfig.java
+++ src/main/java/com/example/project2/Config/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.example.project2.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 }
