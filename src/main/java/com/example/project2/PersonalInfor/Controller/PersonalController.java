89fd270aa782b9e1b64d9ab2c07b07c393806880
kien@kien <kien.nguyen@sotatek.com>
2021-06-01 02:51:37 +0700

    finish feature admin

--- src/main/java/com/example/project2/PersonalInfor/Controller/PersonalController.java
+++ src/main/java/com/example/project2/PersonalInfor/Controller/PersonalController.java
@@ -1,24 +1,29 @@
 package com.example.project2.PersonalInfor.Controller;
 
+import com.example.project2.Commom.Exception.IdNotFoundException;
 import com.example.project2.PersonalInfor.Entity.PersonalEntity;
+import com.example.project2.PersonalInfor.Model.Request.PersonalRequest;
 import com.example.project2.PersonalInfor.Model.Response.PersonalResponse;
+import com.example.project2.PersonalInfor.Repository.PersonalRepository;
 import com.example.project2.PersonalInfor.Service.Impl.PersonalServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/personalInfo")
 public class PersonalController {
     @Autowired
     PersonalServiceImpl personalService;
 
-    @GetMapping("/api/personalInfo/{accountId}")
+    @Autowired
+    PersonalRepository personalRepository;
+
+    @GetMapping("/{accountId}")
     public ResponseEntity<?> getPersonalInfor(@PathVariable Integer accountId) {
         PersonalEntity personalEntity = personalService.findByAccountId(accountId).get();
 //        return ResponseEntity.ok(new PersonalResponse(personalEntity.getFirstName(), personalEntity.getLastName(),
@@ -26,5 +31,23 @@ public class PersonalController {
 //                personalEntity.getReligion(), personalEntity.getPhoneNumber(), personalEntity.getEmail()
 //                ));
         return ResponseEntity.ok(personalEntity);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<?> changeAccountInfor(@RequestBody PersonalEntity personalEntity) throws IdNotFoundException {
+        if(personalRepository.findById(personalEntity.getId()).isPresent()) {
+            personalRepository.save(personalEntity);
+            return ResponseEntity.ok("Success");
+        }
+        throw new IdNotFoundException(personalEntity.getId());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addAccountInfor(@RequestBody PersonalRequest personalRequest) {
+        ModelMapper modelMapper = new ModelMapper();
+        PersonalEntity personalEntity = modelMapper.map(personalRequest, PersonalEntity.class);
+        System.out.println(personalEntity);
+        personalRepository.save(personalEntity);
+        return ResponseEntity.ok("success");
     }
 }
--- src/main/java/com/example/project2/PersonalInfor/Model/Request/PersonalRequest.java
+++ src/main/java/com/example/project2/PersonalInfor/Model/Request/PersonalRequest.java
@@ -1,4 +1,30 @@
 package com.example.project2.PersonalInfor.Model.Request;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
+@Data
 public class PersonalRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer gender;
+
+    private Timestamp dateOfBirth;
+
+    private String address;
+
+    private String ethnicity;
+
+    private String religion;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private Integer accountId;
 }
